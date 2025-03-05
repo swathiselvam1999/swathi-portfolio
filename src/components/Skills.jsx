585e7b4dc7addf7d787980d1bd41b875fff95758
swathiselvam1999 <swathiselvam.bca19@gmail.com>
2025-03-05 18:57:44 +0530

    updated globe

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import SkillGlobe from "./SkillGlobe";
 
 const Skills = () => {
   return (
-    <section id="skills" className="relative z-10 py-20 text-white bg-gray-900 flex flex-col items-center justify-center w-1/2 mx-auto h-screen max-w-[90vw]">
+    <section id="skills" className="relative z-10 py-20 text-white bg-gray-900 flex flex-col items-center justify-center w-3/4 mx-auto h-screen max-w-[90vw]">
       <h3 className="text-4xl font-bold bg-gradient-to-r from-blue-500 to-blue-300 text-transparent bg-clip-text mb-10">My Skills</h3>
       <SkillGlobe />
     </section>
